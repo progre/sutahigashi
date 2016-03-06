527ad24ddadbc6d9fb0e46c12c8596b5a4b8d968
progre <djyayutto@gmail.com>
2016-03-07 07:56:02 +0900

    WIP

--- src/public/js/component/result.tsx
+++ src/public/js/component/result.tsx
@@ -4,6 +4,7 @@ import {RESOURCES} from "./game/player";
 export default class Result extends React.Component<any, any> {
     render() {
         return <div>
+            <img src="res/result.png"/>
             <img src={RESOURCES[this.props.number].src}/>
         </div>;
     }
