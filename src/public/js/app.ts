d381c0622dfe2c35e67c9c907b7d7b29a2e0eaf2
progre <djyayutto@gmail.com>
2016-03-13 07:33:40 +0900

    Refactor

--- src/public/js/app.ts
+++ src/public/js/app.ts
@@ -1,44 +1,23 @@
 /// <reference path="../../../typings/browser.d.ts" />
 import loadResource from "./infrastructure/loader";
-import SE from "./infrastructure/se";
-import {Scene} from "./scene/scene";
-import createScene from "./scene/scenefactory";
-import {Status} from "../../domain/status";
-import {VERSION} from "../../domain/version";
+import direct from "./scene/director";
+
+let loadPromise = new Promise((resolve, reject) => {
+    window.addEventListener("DOMContentLoaded", function onDOMContentLoaded() {
+        window.removeEventListener("DOMContentLoaded", onDOMContentLoaded);
+        resolve();
+    });
+});
 
 async function main() {
-    await new Promise((resolve, reject) => {
-        window.addEventListener("DOMContentLoaded", function onDOMContentLoaded() {
-            window.removeEventListener("DOMContentLoaded", onDOMContentLoaded);
-            resolve();
-        });
-    });
-    let loader = await loadResource();
-    let port = await (await fetch("./wsport")).text();
-
-    let socket = io(`${location.hostname}:${port}`);
-    socket.on("status", (status: Status) => {
-        if (status.version !== VERSION) {
-            window.location.reload(true);
-        }
-    });
-    let stage = new createjs.Stage("canvas");
-    // 今のステートを調べる
-    let se = new SE();
-    let currentScene = await getCurrentScene(socket);
-    while (true) {
-        currentScene = await currentScene(loader, stage, se, socket);
-    }
-}
-
-async function getCurrentScene(socket: SocketIOClient.Socket): Promise<Scene> {
-    let status = await new Promise<Status>((resolve, reject) => {
-        socket.once("status", (status: Status) => {
-            resolve(status);
-        });
-        socket.emit("getstatus");
-    });
-    return createScene(status.scene);
+    let [port, loader] = await Promise.all([
+        (await fetch("./wsport")).text(),
+        (async () => {
+            await loadPromise;
+            return await loadResource();
+        })()
+    ]);
+    await direct(io(`${location.hostname}:${port}`), loader);
 }
 
 main().catch(e => {
---
+++ src/public/js/scene/director.ts
@@ -0,0 +1,30 @@
+import {Status} from "../../../domain/status";
+import {VERSION} from "../../../domain/version";
+import SE from "../infrastructure/se";
+import {Scene} from "../scene/scene";
+import createScene from "../scene/scenefactory";
+
+export default async function direct(socket: SocketIOClient.Socket, loader: createjs.AbstractLoader) {
+    socket.on("status", (status: Status) => {
+        if (status.version !== VERSION) {
+            window.location.reload(true);
+        }
+    });
+    let stage = new createjs.Stage("canvas");
+    // 今のステートを調べる
+    let se = new SE();
+    let currentScene = await getCurrentScene(socket);
+    while (true) {
+        currentScene = await currentScene(loader, stage, se, socket);
+    }
+}
+
+async function getCurrentScene(socket: SocketIOClient.Socket): Promise<Scene> {
+    let status = await new Promise<Status>((resolve, reject) => {
+        socket.once("status", (status: Status) => {
+            resolve(status);
+        });
+        socket.emit("getstatus");
+    });
+    return createScene(status.scene);
+}
