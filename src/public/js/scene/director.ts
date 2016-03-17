b9e8913f0cb1df2efcfe6840ea2bef57e5fec973
progre <djyayutto@gmail.com>
2016-03-17 12:56:57 +0900

    Refactor

--- src/public/js/scene/director.ts
+++ src/public/js/scene/director.ts
@@ -7,49 +7,26 @@ import Interval from "./interval";
 import Result from "./result";
 
 export default async function direct(socket: SocketIOClient.Socket, loader: createjs.AbstractLoader) {
+    let stage = new createjs.Stage("canvas");
+    let se = new SE();
+    let scene = <Scene>{ name: "", close() { }, update(_: any) { } };
     socket.on("status", (status: Status) => {
         if (status.version !== VERSION) {
             window.location.reload(true);
         }
-    });
-    let stage = new createjs.Stage("canvas");
-    let se = new SE();
-    let status = await getStatus(socket);
-    let scene = createScene(status.scene, loader, stage, se, socket);
-    scene.update((<any>status)[status.scene]);
-    socket.on("status", (status: Status) => {
         if (status.scene !== scene.name) {
             scene.close();
-            scene = createScene(status.scene, loader, stage, se, socket);
+            switch (name) {
+                case "lobby": scene = new Lobby(loader, se, socket);
+                case "game": scene = new Game(loader, stage, se, socket);
+                case "interval": scene = new Interval(loader, se);
+                case "result": scene = new Result(loader, se);
+                default: throw new Error();
+            }
         }
         scene.update((<any>status)[status.scene]);
     });
     socket.emit("getstatus");
-}
-
-function getStatus(socket: SocketIOClient.Socket) {
-    return new Promise<Status>(resolve => {
-        socket.once("status", (status: Status) => {
-            resolve(status);
-        });
-        socket.emit("getstatus");
-    });
-}
-
-function createScene(
-    name: string,
-    loader: createjs.AbstractLoader,
-    stage: createjs.Stage,
-    se: SE,
-    sender: SocketIOClient.Socket
-): Scene {
-    switch (name) {
-        case "lobby": return new Lobby(loader, se, sender);
-        case "game": return new Game(loader, stage, se, sender);
-        case "interval": return new Interval(loader, se);
-        case "result": return new Result(loader, se);
-        default: throw new Error();
-    }
 }
 
 interface Scene {
